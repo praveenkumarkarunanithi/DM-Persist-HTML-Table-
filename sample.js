a8c2059d6ca752aa25c63d1499f8e25e2b1f5f63
praveenkumarkarunanithi <100338903+praveenkumarkarunanithi@users.noreply.github.com>
2023-06-01 16:05:30 +0530

    Add files via upload

--- sample.js
+++ sample.js
@@ -17,42 +17,50 @@ let dataManager = new ej.data.DataManager({
   id: "TestDM",
   ignoreOnPersist: ["onTake", "onSortBy"],
 });
+showSpinner();
 dataManager.executeQuery(new ej.data.Query().take(50)).then((e) => {
   (e.result).forEach((data) => {
     table.appendChild(compiledFunction(data)[0]);
   });
+  hideSpinner();
 });
 
 const btnQuery1 = document.getElementById("QueryTake");
 btnQuery1.addEventListener("click", () => {
-  const query1 = new ej.data.Query().take(10); // Replace with your actual query
+  const query1 = new ej.data.Query().take(10);
+  showSpinner(); // Replace with your actual query
   dataManager.executeQuery(query1).then((e) => {
     table.innerHTML = table.querySelector("tr:first-child").outerHTML; // Clear the existing table content
     (e.result).forEach((data) => {
       table.appendChild(compiledFunction(data)[0]);
     });
+    hideSpinner();
   });
 });
 
 const btnQuery2 = document.getElementById("QuerySort");
 btnQuery2.addEventListener("click", () => {
   const query2 = new ej.data.Query().sortBy("Designation", "descending").take(8); // Replace with your actual query
+  showSpinner();
   dataManager.executeQuery(query2).then((e) => {
     table.innerHTML = table.querySelector("tr:first-child").outerHTML; // Clear the existing table content
     (e.result).forEach((data) => {
       table.appendChild(compiledFunction(data)[0]);
     });
+    hideSpinner();
   });
 });
 
 const btnQuery3 = document.getElementById("QuerySearch");
 btnQuery3.addEventListener("click", () => {
   const query2 = new ej.data.Query().search('PRO', ['Designation']).take(6); // Replace with your actual query
+  showSpinner();
   dataManager.executeQuery(query2).then((e) => {
     table.innerHTML = table.querySelector("tr:first-child").outerHTML; // Clear the existing table content
     (e.result).forEach((data) => {
       table.appendChild(compiledFunction(data)[0]);
     });
+    hideSpinner();
   });
 });
 
@@ -75,19 +83,23 @@ let DM2 = new ej.data.DataManager({
   enablePersistence: true,
   ignoreOnPersist: ["onSelect"],
 });
+showSpinner2();
 let result2 = DM2.executeLocal(new ej.data.Query().take(10));
-result2.result.forEach((data) => {
+result2.forEach((data) => {
   table2.appendChild(compiledFunction2(data)[0]);
 });
+hideSpinner2();
 
 const btnQuery12 = document.getElementById("QueryTake2");
 btnQuery12.addEventListener("click", () => {
   const query1 = new ej.data.Query().take(5); // Replace with your actual query
+  showSpinner2();
   result2 = DM2.executeLocal(query1);
   table2.innerHTML = table2.querySelector("tr:first-child").outerHTML; // Clear the existing table2 content
   result2.forEach((data) => {
     table2.appendChild(compiledFunction2(data)[0]);
   });
+  hideSpinner2();
 });
 
 let predicate = new ej.data.Predicate("CustomerID", "equal", "VINET");
@@ -95,21 +107,25 @@ predicate = predicate.or("CustomerID", "equal", "TOMSP");
 const btnQuery22 = document.getElementById("QueryFilter2");
 btnQuery22.addEventListener("click", () => {
   const query2 = new ej.data.Query().where(predicate).take(3); // Replace with your actual query
+  showSpinner2();
   result2 = DM2.executeLocal(query2);
   table2.innerHTML = table2.querySelector("tr:first-child").outerHTML; // Clear the existing table2 content
   result2.forEach((data) => {
     table2.appendChild(compiledFunction2(data)[0]);
   });
+  hideSpinner2();
 });
 
 const btnQuery32 = document.getElementById("QuerySortasc2");
 btnQuery32.addEventListener("click", () => {
   const query2 = new ej.data.Query().select(["OrderID", "CustomerID"]).take(5); // Replace with your actual query
+  showSpinner2();
   result2 = DM2.executeLocal(query2);
   table2.innerHTML = table2.querySelector("tr:first-child").outerHTML; // Clear the existing table2 content
-  result2.forEach((data) => {
+  result2.result.forEach((data) => {
     table2.appendChild(compiledFunction2(data)[0]);
   });
+  hideSpinner2();
 });
 
 const clear2 = document.getElementById("clear2");
@@ -117,3 +133,27 @@ clear2.addEventListener("click", () => {
   DM2.clearPersistence();
   location.reload();
 });
+
+function showSpinner() {
+  const spinner = document.getElementById("spinner");
+  table.style.display = "none"
+  spinner.style.display = "block";
+}
+
+function hideSpinner() {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "none";
+  table.style.display = "block"
+}
+
+function showSpinner2() {
+  const spinner = document.getElementById("spinner2");
+  table2.style.display = "none"
+  spinner.style.display = "block";
+}
+
+function hideSpinner2() {
+  const spinner = document.getElementById("spinner2");
+  spinner.style.display = "none";
+  table2.style.display = "block"
+}
